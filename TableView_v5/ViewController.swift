f8dbd9249d9a198739aa0781e709fc13d4892159
JackDaw1 <iaroshenko.ga@gmail.com>
2022-12-05 15:03:40 +0700

    Commit message

---
+++ TableView_v5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_v5
+//
+//  Created by Galina Iaroshenko on 05.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
