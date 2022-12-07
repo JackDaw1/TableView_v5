75f4a96452faf44c322fa22e825aae332374a40d
JackDaw1 <iaroshenko.ga@gmail.com>
2022-12-07 12:52:26 +0700

    change constraints in DetailViewController

--- TableView_v5/DetailViewController.swift
+++ TableView_v5/DetailViewController.swift
@@ -8,55 +8,61 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+        
+    var task:Task? {
+            didSet {
+                guard let taskItem = task else {return}
+                if let price = taskItem.price {
+                    priceDetailedLabel.text = " \(price) "
+                }
+            }
+        }
     
-    var task: Task?
+
+    let containerView:UIView = {
+           let view = UIView()
+           view.translatesAutoresizingMaskIntoConstraints = true
+           view.clipsToBounds = true // this will make sure its children do not go out of the boundary
+           return view
+       }()
+       
+       
+    
+    let priceDetailedLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    let draftLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
-          
-        //let task1 = TaskAPI.getTask() // model
+        view.backgroundColor = .green
         
-        var task:Task? {
-                didSet {
-                    guard let taskItem = task else {return}
-                    if let price = taskItem.price {
-                        priceDetailedLabel.text = " \(price) "
-                    }
-                }
-            }
-        
-
-        let containerView:UIView = {
-               let view = UIView()
-               view.translatesAutoresizingMaskIntoConstraints = true
-               view.clipsToBounds = true // this will make sure its children do not go out of the boundary
-               return view
-           }()
-           
-           
-        
-        let priceDetailedLabel:UILabel = {
-            let label = UILabel()
-            label.font = UIFont.boldSystemFont(ofSize: 20)
-            return label
-        }()
-        
-        let draftLabel:UILabel = {
-            let label = UILabel()
-            label.font = UIFont.boldSystemFont(ofSize: 20)
-            return label
-        }()
-        
-        
-        //containerView.addSubview(priceDetailedLabel)
         draftLabel.text = " 111111111 "
-        view.addSubview(priceDetailedLabel)
-        view.addSubview(draftLabel)
+        
+        containerView.addSubview(draftLabel)
+        containerView.addSubview(priceDetailedLabel)
+        
+        view.addSubview(containerView)
 
-
+        
+        containerView.heightAnchor.constraint(equalToConstant:40).isActive = true
+        
+        priceDetailedLabel.topAnchor.constraint(equalTo:self.containerView.topAnchor).isActive = true
+        priceDetailedLabel.leadingAnchor.constraint(equalTo:self.containerView.leadingAnchor).isActive = true
+        priceDetailedLabel.trailingAnchor.constraint(equalTo:self.containerView.trailingAnchor).isActive = true
+            
+        draftLabel.topAnchor.constraint(equalTo:self.priceDetailedLabel.bottomAnchor).isActive = true
+        draftLabel.leadingAnchor.constraint(equalTo:self.containerView.leadingAnchor).isActive = true
+      
     }
 
 }
